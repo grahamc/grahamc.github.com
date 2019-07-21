18c2bb9d47c422dfa3e8dc5767d7af5605e51d5e
Graham Christensen <graham@grahamc.com>
2019-07-21 19:28:27 -0400

    lorri post: add

--- default.nix
+++ default.nix
@@ -1,5 +1,15 @@
 let
   pkgs = import <nixpkgs> {};
+
+  asciinema-css = pkgs.fetchurl {
+    url = "https://github.com/asciinema/asciinema-player/releases/download/v2.6.1/asciinema-player.css";
+    sha256 = "sha256:1yi45fdps5mjqdwjhqwwzvlwxb4j7fb8451z7s6sdqmi7py8dksj";
+  };
+  asciinema-js = pkgs.fetchurl {
+    url = "https://github.com/asciinema/asciinema-player/releases/download/v2.6.1/asciinema-player.js";
+    sha256 = "sha256:092y2zl51z23jrl6mcqfxb64xaf9f2dx0j8kp69hp07m0935cz2p";
+  };
+
 in
 { stdenv ? pkgs.stdenv, jekyll ? pkgs.jekyll,
   graphviz ? pkgs.graphviz,
@@ -13,11 +23,18 @@ stdenv.mkDerivation {
     renderPhase() { eval "$renderPhase"; }
   '';
 
+  resourcesPhase = ''
+    #cp ${asciinema-css} ./asciinema.css
+    cp ./asci-cust.css ./asciinema.css
+    cp ${asciinema-js} ./asciinema.js
+  '';
+
   renderPhase = ''
     find . -name '*.dot' -print0 | xargs -0 -n1 -P$NIX_BUILD_CORES ./render.sh
   '';
 
   buildPhase = ''
+    eval "$resourcesPhase"
     eval "$renderPhase"
     rm -f result
     rm -rf ./_site
